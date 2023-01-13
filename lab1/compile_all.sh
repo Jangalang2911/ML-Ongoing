5775c3da05d2e3d80d4069aaadae58eed9761e0e
padirmal <padirmal@teach.cs.toronto.edu>
2023-01-13 15:40:29 -0500

    lab1

---
+++ lab1/compile_all.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+gcc -Wall -g -std=gnu99 -o hello hello.c
+gcc -Wall -g -std=gnu99 -o echo_stdin echo_stdin.c
+gcc -Wall -g -std=gnu99 -o echo_arg echo_arg.c
---
+++ lab1/my_commands.sh
@@ -0,0 +1,4 @@
+./echo_arg csc209 > echo_out.txt
+./echo_stdin < ls -l
+./count 210 | wc
+./echo_stdin < echo_stdin.c
